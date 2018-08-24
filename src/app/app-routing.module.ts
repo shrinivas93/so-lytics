a20cf8d4ddc4f794ebc4769a7892b4779f5e735d
Shrinivas Shukla <ssshukla1993@gmail.com>
2018-08-25 02:05:26 +0530

    Basic Layout Structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/main/about/about.component';
 
 const routes: Routes = [
   {
     path: '',
-    children: []
+    component: AboutComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './components/app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { AboutComponent } from './components/main/about/about.component';
+import { MenuComponent } from './components/main/menu/menu.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, HeaderComponent, FooterComponent, MainComponent, AboutComponent, MenuComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
